a8cfd4db52a1576169c0ba87713457714449544d
Alessio <alessiobellan@proton.me>
2026-07-16 20:31:14 +0200

    chore: switch to HashRouter and set relative base path for static site deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { lazy, Suspense } from 'react';
 import { ScrollToTop } from './components/ScrollToTop';
